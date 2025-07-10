505fa2cf22989590c44cf67708a6e1344a87990f
Ersss01 <etursynbek326@gmail.com>
2025-07-10 19:10:50 +0500

    Исправлен адрес API на https://curtain-on9z.onrender.com

--- src/OrderForm.js
+++ src/OrderForm.js
@@ -17,7 +17,7 @@ export default function OrderForm() {
     e.preventDefault();
     setError('');
     try {
-      const res = await fetch('http://localhost:5000/api/order', {
+      const res = await fetch('https://curtain-on9z.onrender.com/api/order', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form),
